d098eb365cf495979f81a9642f8905b5897a26d7
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-01-15 00:54:15 -0300

    Add Vuetify

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import pt from 'vuetify/es5/locale/pt'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  }
+})
